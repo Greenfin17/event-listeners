9d59fb22496035fb481f23b368b410ed8334505a
Greenfin17 <jmaple@yahoo.com>
2021-01-23 11:46:49 -0600

    practiced with event listeners

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 console.log("Connected");
 
-const lessons = {
-  1: "document",
-  2: "callbacks",
-  3: "event listeners",
-  4: "Bootstrap",
-  5: "Code"
-}
+const container = document.querySelector('#container');
 
 const button1 = document.getElementById('main-btn');
-
 console.log(button1);
 
-const q_button1 = document.querySelector('#main-btn');
+// button.addEventListener('click', (event) => {
+//   //console.log('You clicked me!');
+//   console.log(`You clicked me ${event.target.id}!`);
+//   console.log(event);
+// });
 
-console.log(q_button1);
+const whatIsTheId = (event) => {
+  console.log(`YOU CLICKED ${event.target.id}!`);
+  
+  if(event.target.id === 'main-btn') {
+    container.innerHTML = "You clicked the Main button";
+  } else {
+    container.innerHTML = "You clicked the Other button";
+  }
+}
+
+const button2 = document.getElementById('second-btn');
+
+button1.addEventListener('click', whatIsTheId);
+button2.addEventListener('click', whatIsTheId);
