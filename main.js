7d30a561ec9e9cd823ff2680c8d23161e1669484
Greenfin17 <jmaple@yahoo.com>
2021-01-23 10:57:18 -0600

    selectors

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ const lessons = {
   5: "Code"
 }
 
+const button1 = document.getElementById('main-btn');
 
+console.log(button1);
+
+const q_button1 = document.querySelector('#main-btn');
+
+console.log(q_button1);
