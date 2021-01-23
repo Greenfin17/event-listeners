1576bfcfe7125cabae21054cad95c660b0df6130
Greenfin17 <jmaple@yahoo.com>
2021-01-23 10:02:44 -0600

    tested for in with array

--- main.js
+++ main.js
@@ -1,1 +1,68 @@
 console.log("Connected");
+
+const firstObject = {};
+
+const user = {
+  userName: 'biz.markies',
+  password: 'abc123',
+  lovesJavascript: true,
+  favoriteNumber: 42,
+}
+
+// DOT NOTATION
+// console.log(user.lovesJavascript);
+// console.log(user.userName);
+// // BRACKET NOTATION 
+
+// const password = 'password';
+// console.log(user['password']);
+// console.log(user['userName']);
+// console.log(user['favoriteNumber']);
+// console.log(user.favoriteNumber);
+
+// ASSIGNING VALUES
+
+const newUser = {
+  isNew: true,
+}
+
+// newUser.username = 'fresh.prince';
+// console.log(newUser);
+// newUser['password'] = 'winter2021'; 
+// console.log(newUser);
+
+const newObject = {
+  username: 'John',
+  sayHello: function () {
+    console.log(`${this.username} says Hello!`);
+  } 
+}
+
+//console.log(newObject.sayHello);
+// newObject.sayHello();
+// newObject.username = 'Aja';
+// newObject.sayHello();
+
+// ITERATE OVER AN OBJECT
+
+const userTwo = {
+  username: 'trinity.christiana',
+  password: 'xyz0987',
+  lovesJavascript: true,
+  favoriteNumber: 12,
+}
+
+// for(let key in userTwo) {
+//   console.log(key);
+// }
+
+for(let key in userTwo) {
+//  console.log(key);
+  console.log(userTwo[key]);
+  //console.log(userTwo.key); // undefined
+}
+
+const stuff = ['1', 'hello', 4, 5];
+for(let item in stuff) {
+  console.log(stuff[item]);
+}
